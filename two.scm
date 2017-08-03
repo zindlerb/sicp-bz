e8751a24fc67a4679d08fc15134601283cf5a336
Brian Zindler <bzindler@Brians-MacBook-Pro.local>
2017-08-02 23:19:29 -0700

    programmin

--- two.scm
+++ two.scm
@@ -232,3 +232,32 @@
 	 (and (= (accumulate + perm) s))
 	      (all (lambda (i) (< i n)) perm)))
   (permutations (list i k j)))
+
+
+;; 2.3.3
+
+(define (element-of-set? x set)
+  (cond ((null? set) false)
+        ((equal? x (car set)) true)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set
+      (cons x set)))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1) (null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+(define (union-set set1 set2)
+	(define (loop set1 set2)
+	  (cond ((or (null? set1) (null? set2)) '())
+			((not (element-of-set? (car set1) set2))
+			 (cons (car set1)
+				   (loop (cdr set1) set2)))
+			(else (loop (cdr set1) set2))))
+	(append (loop set1 set2) set2))
