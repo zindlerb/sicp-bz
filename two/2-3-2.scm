1c7033a796a26aef6bafbc9e5bed63fd38579a54
Brian Zindler <bzindler@Brians-MacBook-Pro.local>
2017-08-11 16:22:10 -0700

    finish 1 ex

--- two/2-3-2.scm
+++ two/2-3-2.scm
@@ -1,19 +1,3 @@
-(define (deriv exp var)
-  (cond ((number? exp) 0)
-        ((variable? exp)
-         (if (same-variable? exp var) 1 0))
-        ((sum? exp)
-         (make-sum (deriv (addend exp) var)
-                   (deriv (augend exp) var)))
-        ((product? exp)
-         (make-sum
-		  (make-product (multiplier exp)
-						(deriv (multiplicand exp) var))
-		  (make-product (deriv (multiplier exp) var)
-						(multiplicand exp))))
-        (else
-         (error "unknown expression type -- DERIV" exp))))
-
 (define (variable? x) (symbol? x))
 
 (define (same-variable? v1 v2)
@@ -34,7 +18,7 @@
 							'(0) args))))
 	(if (eq? (length filtered-args) 1)
 		(car filtered-args)
-		(list '+ filtered-args))))
+		(cons '+ filtered-args))))
 
 
 ;; if any are zero it is zero
@@ -42,16 +26,18 @@
 (define (make-product . args)
   (if (any (lambda (x) (and (number? x) (=number? 0 x))) args)
 	0
-	(let ((filtered-args (fold-right
-		(lambda (val n-and-syms)
-			(if (number? val)
-				(cons (* val (car n-and-syms))
-					  (cdr n-and-syms))
-				(cons (car n-and-syms)
-					  (cons val (cdr n-and-syms))))) '(1) args)))
+	(let ((filtered-args
+		   (filter (lambda (x) (not (=number? x 1)))
+			(fold-right
+				(lambda (val n-and-syms)
+					(if (number? val)
+						(cons (* val (car n-and-syms))
+							  (cdr n-and-syms))
+						(cons (car n-and-syms)
+							  (cons val (cdr n-and-syms))))) '(1) args))))
 	  (if (eq? (length filtered-args) 1)
 		  (car filtered-args)
-		  (list '* filtered-args)))))
+		  (cons '* filtered-args)))))
 
 (define (sum? x)
   (and (pair? x) (eq? (car x) '+)))
@@ -62,7 +48,7 @@
   (let ((cddr-val (cddr s)))
    (if (eq? (length cddr-val) 1)
 		(car cddr-val)
-		(list '+ cddr-val))))
+		(cons '+ cddr-val))))
 
 (define (product? x)
   (and (pair? x) (eq? (car x) '*)))
@@ -73,7 +59,7 @@
   (let ((cddr-val (cddr p)))
 	(if (eq? (length cddr-val) 1)
 		(car cddr-val)
-		(list '* cddr-val))))
+		(cons '* cddr-val))))
 
 ;; Exercise 2.56
 
@@ -111,3 +97,48 @@
 
 
 ;; by adding a new clause to the deriv program and defining appropriate procedures exponentiation?, base, exponent, and make-exponentiation. (You may use the symbol ** to denote exponentiation.) Build in the rules that anything raised to the power 0 is 1 and anything raised to the power 1 is the thing itself.
+(deriv '(* x y (+ x 3)) 'x)
+
+;; Exercise 2.58
+
+;; a
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list a1 '+ a2))))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) (=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) (* m1 m2))
+        (else (list m1 '* m2))))
+
+(define (sum? x)
+  (and (pair? x) (eq? (cadr x) '+)))
+
+(define (addend s) (car s))
+
+(define (augend s) (caddr s))
+
+(define (product? x)
+  (and (pair? x) (eq? (cadr x) '*)))
+
+(define (multiplier p) (car p))
+
+(define (multiplicand p) (caddr p))
+
+(deriv '(x + (3 * (x + (y + 2)))) 'x) ;; 4
+;; b
+;; for be use order of ops to convert over to the list form...
+(deriv '(x + 3 * (x + y + 2)) 'x)
+
+;; every expression should consist of 3
+;; the last should
+
+;; recurse until length three
+;; recurse on children
+;;  put into lists -> sort on operators
+(define (pembdas-to-parens exp)
+	)
