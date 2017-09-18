3e0b8a96f3fab7e51fe941d3c31a9b3108df84aa
Brian Zindler <zindlerb@gmail.com>
2017-09-17 17:23:10 -0700

    full adder

--- three/3-3-4.scm
+++ three/3-3-4.scm
@@ -1,1 +1,285 @@
+;; -*- geiser-scheme-implementation: 'chicken -*-
 
+(define (make-time-segment time queue)
+  (cons time queue))
+(define (segment-time s) (car s))
+(define (segment-queue s) (cdr s))
+
+(define (make-agenda) (list 0))
+(define (current-time agenda) (car agenda))
+(define (set-current-time! agenda time)
+  (set-car! agenda time))
+(define (segments agenda) (cdr agenda))
+(define (set-segments! agenda segments)
+  (set-cdr! agenda segments))
+(define (first-segment agenda) (car (segments agenda)))
+(define (rest-segments agenda) (cdr (segments agenda)))
+
+(define (front-ptr queue) (car queue))
+(define (rear-ptr queue) (cdr queue))
+(define (set-front-ptr! queue item) (set-car! queue item))
+(define (set-rear-ptr! queue item) (set-cdr! queue item))
+
+(define (make-queue) (cons '() '()))
+
+(define (empty-queue? queue) (null? (front-ptr queue)))
+
+(define (delete-queue! queue)
+  (cond ((empty-queue? queue)
+         (error "DELETE! called with an empty queue" queue)) (else
+                                                              (set-front-ptr! queue (cdr (front-ptr queue))) queue)))
+
+(define (front-queue queue)
+  (if (empty-queue? queue)
+      (error "FRONT called with an empty queue" queue)
+      (car (front-ptr queue))))
+
+(define (insert-queue! queue item)
+  (let ((new-pair (cons item '())))
+    (cond ((empty-queue? queue)
+           (set-front-ptr! queue new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue)
+          (else
+           (set-cdr! (rear-ptr queue) new-pair)
+           (set-rear-ptr! queue new-pair)
+           queue))))
+
+(define (make-wire . args)
+  (let ((signal-value (if (null? args) 0 (car args))) (action-procedures '()))
+    (define (set-my-signal! new-value)
+      (if (not (= signal-value new-value))
+          (begin (set! signal-value new-value)
+                 (call-each action-procedures))
+          'done))
+    (define (accept-action-procedure! proc)
+      (set! action-procedures (cons proc action-procedures))
+      (proc))
+    (define (dispatch m)
+      (cond ((eq? m 'get-signal) signal-value)
+            ((eq? m 'set-signal!) set-my-signal!)
+            ((eq? m 'add-action!) accept-action-procedure!)
+            (else (error "Unknown operation -- WIRE" m))))
+    dispatch))
+
+(define (call-each procedures)
+  (if (null? procedures)
+      'done
+      (begin
+        ((car procedures))
+        (call-each (cdr procedures)))))
+
+(define (get-signal wire)
+  (wire 'get-signal))
+(define (set-signal! wire new-value)
+  ((wire 'set-signal!) new-value))
+(define (add-action! wire action-procedure)
+  ((wire 'add-action!) action-procedure))
+
+(define (after-delay delay action)
+  (add-to-agenda! (+ delay (current-time the-agenda))
+                  action
+                  the-agenda))
+
+(define (propagate)
+  (if (empty-agenda? the-agenda)
+      'done
+      (let ((first-item (first-agenda-item the-agenda)))
+        (first-item)
+        (remove-first-agenda-item! the-agenda)
+        (propagate))))
+
+(define (probe name wire)
+  (add-action! wire
+               (lambda ()
+                 (newline)
+                 (display name)
+                 (display " ")
+                 (display (current-time the-agenda))
+                 (display "  New-value = ")
+                 (display (get-signal wire)))))
+
+
+
+(define (empty-agenda? agenda)
+  (null? (segments agenda)))
+
+(define (add-to-agenda! time action agenda)
+  (define (belongs-before? segments)
+    (or (null? segments)
+        (< time (segment-time (car segments)))))
+  (define (make-new-time-segment time action)
+    (let ((q (make-queue)))
+      (insert-queue! q action)
+      (make-time-segment time q)))
+  (define (add-to-segments! segments)
+    (if (= (segment-time (car segments)) time)
+        (insert-queue! (segment-queue (car segments))
+                       action)
+        (let ((rest (cdr segments)))
+          (if (belongs-before? rest)
+              (set-cdr!
+               segments
+               (cons (make-new-time-segment time action)
+                     (cdr segments)))
+              (add-to-segments! rest)))))
+  (let ((segments (segments agenda)))
+    (if (belongs-before? segments)
+        (set-segments!
+         agenda
+         (cons (make-new-time-segment time action)
+               segments))
+        (add-to-segments! segments))))
+
+(define (remove-first-agenda-item! agenda)
+  (let ((q (segment-queue (first-segment agenda))))
+    (delete-queue! q)
+    (if (empty-queue? q)
+        (set-segments! agenda (rest-segments agenda)))))
+
+(define (first-agenda-item agenda)
+  (if (empty-agenda? agenda)
+      (error "Agenda is empty -- FIRST-AGENDA-ITEM")
+      (let ((first-seg (first-segment agenda)))
+        (set-current-time! agenda (segment-time first-seg))
+        (front-queue (segment-queue first-seg)))))
+
+
+
+(define (inverter input output)
+  (define (invert-input)
+    (let ((new-value (logical-not (get-signal input))))
+      (after-delay inverter-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! input invert-input)
+  'ok)
+
+(define (logical-not s)
+  (cond ((= s 0) 1)
+        ((= s 1) 0)
+        (else (error "Invalid signal" s))))
+
+(define (logical-and a b)
+  (cond ((and (= a 1) (= b 1)) 1)
+        (else 0)))
+
+(define (logical-or a b)
+  (cond ((= a 1) 1)
+        ((= b 1) 1)
+        (else 0)))
+
+(define (and-gate a1 a2 output)
+  (define (and-action-procedure)
+    (let ((new-value
+           (logical-and (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 and-action-procedure)
+  (add-action! a2 and-action-procedure)
+  'ok)
+
+(define the-agenda (make-agenda))
+(define inverter-delay 2)
+(define and-gate-delay 3)
+(define or-gate-delay 5)
+
+(define (half-adder a b s c)
+  (let ((d (make-wire)) (e (make-wire)))
+    (or-gate a b d)
+    (and-gate a b c)
+    (inverter c e)
+    (and-gate d e s)
+    'ok))
+
+(define (full-adder a b c-in sum c-out)
+  (let ((s (make-wire))
+        (c1 (make-wire))
+        (c2 (make-wire)))
+    (half-adder b c-in s c1)
+    (half-adder a s sum c2)
+    (or-gate c1 c2 c-out)
+    'ok))
+
+;; Exercise 3.28
+(define input-1 (make-wire))
+(define input-2 (make-wire))
+(define output-or (make-wire))
+
+(define (or-gate a1 a2 output)
+  (define (or-action-procedure)
+    (let ((new-value
+           (logical-or (get-signal a1) (get-signal a2))))
+      (after-delay or-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 or-action-procedure)
+  (add-action! a2 or-action-procedure)
+  'ok)
+
+;; Exercise 3.29
+(define (or-gate-2 a1 a2 output)
+  (let ((i-a1 (make-wire))
+        (i-a2 (make-wire))
+        (and-out (make-wire)))
+    (inverter a1 i-a1)
+    (inverter a2 i-a2)
+    (and-gate i-a1 i-a2 and-out)
+    (inverter and-out output))
+  'ok)
+
+(or-gate-2 input-1 input-2 output-or)
+(probe "or-output" output-or)
+
+(set-signal! input-1 1)
+(propagate)
+
+;; Exercise 3.30
+
+(define (ripple-carry a-list b-list result-list final-carry)
+  (define (loop a-list b-list result-list prev-carry)
+    (if (null? a-list)
+        prev-carry
+        (let ((a (car a-list))
+              (b (car b-list))
+              (result (car result-list))
+              (new-carry (if (null? (cdr a-list)) final-carry (make-wire))))
+          (full-adder a b prev-carry result new-carry)
+          (loop (cdr a-list) (cdr b-list) (cdr result-list) new-carry))))
+  (loop
+   a-list
+   b-list
+   result-list
+   (make-wire)))
+
+;;   1 +   7 =   8
+;; 100 + 111 = 000 c1
+
+(define a1 (make-wire 1))
+(define a2 (make-wire))
+(define a3 (make-wire))
+
+(define a-list (list a1 a2 a3))
+
+(define b1 (make-wire 1))
+(define b2 (make-wire 1))
+(define b3 (make-wire 1))
+
+(define b-list (list b1 b2 b3))
+
+(define r1 (make-wire))
+(define r2 (make-wire))
+(define r3 (make-wire))
+
+(define r-list (list r1 r2 r3))
+
+(define c (make-wire))
+
+(probe "r1" r1)
+(probe "r2" r2)
+(probe "r3" r3)
+(probe "c" c)
+
+(ripple-carry a-list b-list r-list c)
+(propagate)
